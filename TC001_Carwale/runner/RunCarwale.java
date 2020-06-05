eadfc3a4f61187e59b709fb30a25b077439a1f81
SowmyMano <63657762+SowmyMano@users.noreply.github.com>
2020-06-05 22:01:24 +0530

    Add files via upload

--- TC001_Carwale/runner/RunCarwale.java
+++ TC001_Carwale/runner/RunCarwale.java
@@ -1,1 +1,12 @@
+package runner;
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+//import io.cucumber.testng.CucumberOptions.SnippetType;
+
+@CucumberOptions(features = "src/test/java/feature/Carwale.feature", glue = "steps", monochrome = true)
+//					, dryRun = true, snippets = SnippetType.CAMELCASE)
+
+public class RunCarwale extends AbstractTestNGCucumberTests{
+
+}
